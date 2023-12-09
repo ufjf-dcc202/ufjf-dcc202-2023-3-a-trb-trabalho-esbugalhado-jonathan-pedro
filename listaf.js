ace7aa918664cf2ddb125e0073a95ca9bf3530da
opedrobonfa <pedro.bonfa@estudante.ufjf.br>
2023-12-09 17:48:36 -0300

    cria listf.js para funções e adiciona matriz 3X3

---
+++ listaf.js
@@ -0,0 +1,13 @@
+
+let a1=0,a2=0,a3=0,a4=0,a5=0,a6=0,a7=0,a8=0,a9=0;
+let linha1 = [a1, a4, a7];
+let linha2 = [a2, a5, a8];
+let linha3 = [a3, a6, a9];
+let coluna1 = [];
+let coluna2 = [];
+let coluna3 = [];
+let dadosPlayer = [linha1, linha2, linha3];
+
+
+console.log(dadosPlayer);
+
