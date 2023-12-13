4b0907f3a40d045998b16c25a97f1e9ac9db545b
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2023-12-13 10:27:33 -0300

    cria funções pra cálculo de combo no bot e imprime matrizBot

--- listaf.js
+++ listaf.js
@@ -58,7 +58,7 @@ function colocaValorNasColunas()
     coluna3 = [a7,a8,a9];
 }
 
-//valores dos combos (se houver)
+//valores dos combos (se houver) [player]
 function verificaCombosC1()
 {
     let valorCombo = 0;
@@ -92,66 +92,75 @@ function verificaCombosC1()
 
 function verificaCombosC2()
 {
-    let valorCombo = 0;
+    let valorCombo2 = 0;
 
     if(coluna2[0] == coluna2[1])
     {
-        valorCombo = ((coluna2[0]*coluna2[1])*2)+coluna2[2];
+        valorCombo2 = ((coluna2[0]*coluna2[1])*2)+coluna2[2];
     }
 
     else if ( coluna2[0] == coluna2[2]) 
     {
-        valorCombo = ((coluna2[0]*coluna2[2])*2)+coluna2[1];
+        valorCombo2 = ((coluna2[0]*coluna2[2])*2)+coluna2[1];
     }
     
     else if ( coluna2[1] == coluna2[2]) 
     {
-        valorCombo = ((coluna2[1]*coluna2[2])*2)+coluna2[0];
+        valorCombo2 = ((coluna2[1]*coluna2[2])*2)+coluna2[0];
     }
 
     else if (coluna2[0] == coluna2[1] && coluna2[0] == coluna2[2])
     {
-        valorCombo = (coluna2[0]*coluna2[1]*coluna2[2])*3;
+        valorCombo2 = (coluna2[0]*coluna2[1]*coluna2[2])*3;
     }
 
     else
     {
-        valorCombo = coluna2[0] + coluna2[1] + coluna2[2]
+        valorCombo2 = coluna2[0] + coluna2[1] + coluna2[2]
     }
-    return valorCombo;
+    return valorCombo2;
 }
 
 function verificaCombosC3()
 {
-    let valorCombo = 0;
+    let valorCombo3 = 0;
 
     if(coluna3[0] == coluna3[1])
     {
-        valorCombo = ((coluna3[0]*coluna3[1])*2)+coluna3[2];
+        valorCombo3 = ((coluna3[0]*coluna3[1])*2)+coluna3[2];
     }
 
     else if ( coluna3[0] == coluna3[2]) 
     {
-        valorCombo = ((coluna3[0]*coluna3[2])*2)+coluna3[1];
+        valorCombo3 = ((coluna3[0]*coluna3[2])*2)+coluna3[1];
     }
     
     else if ( coluna3[1] == coluna3[2]) 
     {
-        valorCombo = ((coluna3[1]*coluna3[2])*2)+coluna3[0];
+        valorCombo3 = ((coluna3[1]*coluna3[2])*2)+coluna3[0];
     }
 
     else if (coluna3[0] == coluna3[1] && coluna3[0] == coluna3[2])
     {
-        valorCombo = (coluna3[0]*coluna3[1]*coluna3[2])*3;
+        valorCombo3 = (coluna3[0]*coluna3[1]*coluna3[2])*3;
     }
 
     else
     {
-        valorCombo = coluna3[0] + coluna3[1] + coluna3[2]
+        valorCombo3 = coluna3[0] + coluna3[1] + coluna3[2]
     }
-    return valorCombo;
+    return valorCombo3;
 }
 
+let b1 = 0;
+let b2 = 0;
+let b3 = 0;
+let b4 = 0;
+let b5 = 0;
+let b6 = 0;
+let b7 = 0;
+let b8 = 0;
+let b9 = 0;
 //matriz bot
 let linhab1 = [b1, b4, b7];
 let linhab2 = [b2, b5, b8];
@@ -165,13 +174,13 @@ function acessaColunasMaq()
 {
 
     for(let i = 0; i < 3; i++){
-        colunab1[i] = dadosPlayer[i][0];//acessa primeira coluna
+        colunab1[i] = dadosBot[i][0];//acessa primeira coluna
     }
     for(let i = 0; i < 3; i++){
-        colunab2[i] = dadosPlayer[i][1];//acessa segunda coluna
+        colunab2[i] = dadosBot[i][1];//acessa segunda coluna
     }
     for(let i = 0; i < 3; i++){
-        colunab3[i] = dadosPlayer[i][2];//acessa terceira coluna
+        colunab3[i] = dadosBot[i][2];//acessa terceira coluna
     }
 }
 
@@ -187,9 +196,105 @@ function dadoMaq()
     numDadoNoTurno();
 }
 
+//valores dos combos (se houver) [bot]
 
+function verificaCombosM1()
+{
+    let valorComboM = 0;
+
+    if(colunab1[0] == colunab1[1])
+    {
+        valorComboM = ((colunab1[0]+colunab1[1])*2)+colunab1[2];
+    }
+
+    else if ( colunab1[0] == colunab1[2]) 
+    {
+        valorComboM = ((colunab1[0]+colunab1[2])*2)+colunab1[1];
+    }
+    
+    else if ( colunab1[1] == colunab1[2]) 
+    {
+        valorComboM = ((colunab1[1]+colunab1[2])*2)+colunab1[0];
+    }
+
+    else if (colunab1[0] == colunab1[1] && colunab1[0] == colunab1[2])
+    {
+        valorComboM = (colunab1[0]+colunab1[1]+colunab1[2])*3;
+    }
+
+    else
+    {
+        valorComboM = colunab1[0] + colunab1[1] + colunab1[2];
+    }
+    return valorComboM;
+}
+
+function verificaCombosM2()
+{
+    let valorComboM = 0;
+
+    if(colunab2[0] == colunab2[1])
+    {
+        valorComboM = ((colunab2[0]+colunab2[1])*2)+colunab2[2];
+    }
+
+    else if ( colunab2[0] == colunab2[2]) 
+    {
+        valorComboM = ((colunab2[0]+colunab2[2])*2)+colunab2[1];
+    }
+    
+    else if ( colunab2[1] == colunab2[2]) 
+    {
+        valorComboM = ((colunab2[1]+colunab2[2])*2)+colunab2[0];
+    }
+
+    else if (colunab2[0] == colunab2[1] && colunab2[0] == colunab2[2])
+    {
+        valorComboM = (colunab2[0]+colunab2[1]+colunab2[2])*3;
+    }
+    
+    else
+    {
+        valorComboM = colunab2[0] + colunab2[1] + colunab2[2];
+    }
+    return valorComboM;
+}
+
+function verificaCombosM3()
+{
+    let valorComboM = 0;
+
+    if(colunab3[0] == colunab3[1])
+    {
+        valorComboM = ((colunab3[0]+colunab3[1])*2)+colunab3[2];
+    }
+
+    else if ( colunab3[0] == colunab3[2]) 
+    {
+        valorComboM = ((colunab3[0]+colunab3[2])*2)+colunab3[1];
+    }
+    
+    else if ( colunab3[1] == colunab3[2]) 
+    {
+        valorComboM = ((colunab3[1]+colunab3[2])*2)+colunab3[0];
+    }
+
+    else if (colunab3[0] == colunab3[1] && colunab3[0] == colunab3[2])
+    {
+        valorComboM = (colunab3[0]+colunab3[1]+colunab3[2])*3;
+    }
+    
+    else
+    {
+        valorComboM = colunab3[0] + colunab3[1] + colunab3[2];
+    }
+    return valorComboM;
+}
 acessaColunas();
 colocaValorNasColunas();
 atualizaValoresMatriz();
-console.log(colunab1);
+acessaColunasMaq();
+colocaValorNasColunasMaq();
+console.log(coluna1, coluna2, coluna3);
+console.log(colunab1, colunab2, colunab3);
 
