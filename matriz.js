b06da477c3003aed6cba7a0c8a1cef901f95dd0d
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2023-12-15 15:02:03 -0300

    implementa getMatriz para chamar matriz em main.js

--- matriz.js
+++ matriz.js
@@ -1,4 +1,4 @@
-//matriz player
+
 let a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0, a6 = 0, a7 = 0, a8 = 0, a9 = 0;
 let linha1 = [a1, a4, a7];
 let linha2 = [a2, a5, a8];
@@ -6,7 +6,13 @@ let linha3 = [a3, a6, a9];
 let coluna1 = [];
 let coluna2 = [];
 let coluna3 = [];
-let dadosPlayer = [linha1, linha2, linha3];
+const dadosPlayer = [linha1, linha2, linha3];
+
+export function getMatriz()
+{
+    return dadosPlayer;
+}
+
 
 export function numDadoNoTurno()
 {
