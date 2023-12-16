525ad5350eea5b0da4712a5eaf143e0749bd6857
opedrobonfa <pedro.bonfa@estudante.ufjf.br>
2023-12-16 19:55:10 -0300

    adiciona matriz do bot e do player em main, e função para zerá-las

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-import {getMatriz, colocaValorNasColunas, numDadoNoTurno, atualizaValoresMatriz} from "./matriz.js";
+import {getMatrizPlayer, getMatrizBot,colocaValorNasColunas, numDadoNoTurno, atualizaValoresMatriz, verificaCombosC1, verificaCombosC2, verificaCombosC3} from "./matriz.js";
 
-getMatriz();
+
+let matrizBot = getMatrizBot();
+let matrizPlayer = getMatrizPlayer();
+
+function zeraMatriz(matriz){  
+    for(let i =0; i <3;i++)
+        {
+            for(let l = 0; l<3;l++)
+            {
+                matriz[i][l] = 0;    
+            }
+        }
+}
+
+zeraMatriz(matrizBot);
+zeraMatriz(matrizPlayer);
+
 
 function rolaDadoBot()
 {
@@ -13,24 +29,12 @@ function rolaDadoBot()
     return resultado;
 }
 
-function colocaNaLinha(numeroLinha, valor)
-{
-    atualizaValoresMatriz();
-    colocaValorNasColunas();
-    if (numeroLinha >= 0 && numeroLinha < 3) {
-        dadosPlayer[numeroLinha][0] = valor; // Define o valor na coluna1[0] da linha desejada
-        atualizaValoresMatriz();
-    }
-}   
-
 
 function turnoDoBot() //primeiro turno
 {
-    let resultado = numDadoNoTurno();
-    rolaDadoBot();
-    colocaNaLinha(0, resultado);
+    let dado = rolaDadoBot();
+    verificaLinhaB(matrizBot, dado);
+
 }
 
-
 turnoDoBot();
-
