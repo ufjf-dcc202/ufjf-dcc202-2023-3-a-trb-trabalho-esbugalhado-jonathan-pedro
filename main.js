c344b27ce31963d28679aa8595976b3473073dcb
opedrobonfa <pedro.bonfa@estudante.ufjf.br>
2023-12-17 11:37:19 -0300

    cria funções de soma para player e bot ao final de cada turno

--- main.js
+++ main.js
@@ -76,14 +76,13 @@ function verificaLinhaPlayerC1(matriz, dadoNaRodada)
     }
 }
 
-
-
-function verificaLinhaPlayerC2(matriz, dadoNaRodada){
+function verificaLinhaPlayerC2(matriz, dadoNaRodada)
+{
     if(matriz[1][0] === 0)
     {
-            matriz[1][0] = dadoNaRodada;
-            document.getElementById("a4").innerHTML= dadoNaRodada;
-        }
+        matriz[1][0] = dadoNaRodada;
+        document.getElementById("a4").innerHTML= dadoNaRodada;
+    }
         else if(matriz[1][1] === 0)
         {
             matriz[1][1] = dadoNaRodada;
@@ -93,46 +92,49 @@ function verificaLinhaPlayerC2(matriz, dadoNaRodada){
         {
             matriz[1][2] = dadoNaRodada;
             document.getElementById("a6").innerHTML= dadoNaRodada;
-        }
-    }
+        }   
+}
     
-    function verificaLinhaPlayerC3(matriz, dadoNaRodada)
+function verificaLinhaPlayerC3(matriz, dadoNaRodada)
+{
+    if(matriz[2][0] === 0)
     {
-        if(matriz[2][0] === 0)
-        {
-            matriz[2][0] = dadoNaRodada;
-            document.getElementById("a7").innerHTML= dadoNaRodada;
-        }
-        else if(matriz[2][1] === 0)
-        {
-            matriz[2][1] = dadoNaRodada;
-            document.getElementById("a8").innerHTML= dadoNaRodada;
-        }
-        else if(matriz[2][2] === 0)
-        {
-            matriz[2][2] = dadoNaRodada;
-            document.getElementById("a9").innerHTML= dadoNaRodada;
-        }
-        
+        matriz[2][0] = dadoNaRodada;
+        document.getElementById("a7").innerHTML= dadoNaRodada;
     }
-    
-    function escolhaPlayer(dado)
+    else if(matriz[2][1] === 0)
     {
-        let col1 = document.getElementById("colunaInf1");
-        col1.addEventListener("click", function(){
-            verificaLinhaPlayerC1(matrizPlayer, dado);
-        });
-    
-        let col2 = document.getElementById("colunaInf2");
-        col2.addEventListener("click", function(){
-            verificaLinhaPlayerC2(matrizPlayer, dado);
-        });
-    
-        let col3 = document.getElementById("colunaInf3");
-        col3.addEventListener("click", function(){
-            verificaLinhaPlayerC3(matrizPlayer, dado);
-        });
+        matriz[2][1] = dadoNaRodada;
+        document.getElementById("a8").innerHTML= dadoNaRodada;
     }
+    else if(matriz[2][2] === 0)
+    {
+        matriz[2][2] = dadoNaRodada;
+        document.getElementById("a9").innerHTML= dadoNaRodada;
+    }
+    
+}
+    
+function escolhaPlayer(dado)
+{
+    let col1 = document.getElementById("colunaInf1");
+    col1.addEventListener("click", function(){
+        verificaLinhaPlayerC1(matrizPlayer, dado);
+        afereSomaPlayer(0,matrizPlayer);
+    });
+
+    let col2 = document.getElementById("colunaInf2");
+    col2.addEventListener("click", function(){
+        verificaLinhaPlayerC2(matrizPlayer, dado);
+        afereSomaPlayer(1,matrizPlayer);
+    });
+
+    let col3 = document.getElementById("colunaInf3");
+    col3.addEventListener("click", function(){
+        verificaLinhaPlayerC3(matrizPlayer, dado);
+        afereSomaPlayer(2,matrizPlayer);
+    });
+}
     
     
     
@@ -146,29 +148,59 @@ function turnoDoPlayer() //primeiro turno
 {
     let dado = rolaDadoPlayer();
     escolhaPlayer(dado);
+    
 
 }
 
-function afereSomaBot()
+function afereSomaBot(pos, matriz) 
 {
-    let soma=[[a], [b], [c]];
-    for (let i=0;i<3;i++)
+    let soma=0;
+    
+  for(let j=0;j<3;j++)
     {
-        for(let j=0;j<3;j++)
-        {
-            soma[0] = matriz[0][j] + matriz[0][j];
-        }
+        soma += matriz[pos][j];
     }
+
+  if (pos === 0){
+        let t1 = document.getElementById("t1");
+        t1.innerHTML = soma;
+    }
+    
+}
+
+function afereSomaPlayer(pos, matriz) 
+{
+    let soma=0;
+    
+  for(let j=0;j<3;j++)
+    {
+        soma += matriz[pos][j];
+    }
+
+    if (pos === 0){
+            let t4 = document.getElementById("t4");
+            t4.innerHTML = soma;
+        }
+    if (pos === 1){
+        let t5 = document.getElementById("t5");
+        t5.innerHTML = soma;
+    }
+    if (pos === 2){
+        let t6 = document.getElementById("t6");
+        t6.innerHTML = soma;
+    }
+    
 }
 
 function partida()
 {
     zeraMatriz(matrizBot);
     zeraMatriz(matrizPlayer);
+
     turnoDoBot();
-    afereSomaBot();
+    afereSomaBot(0,matrizBot);
     turnoDoPlayer();
-    afereSomaPlayer();
+    
 }
 
 partida();
